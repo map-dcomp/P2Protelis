714b396e18558676664046646d23b382f4aa6da4
Jon Schewe <jon.schewe@raytheon.com>
2017-06-21 08:36:37 -0500

    Add logging of port number when there is a failure to accept

--- src/main/java/com/bbn/protelis/networkresourcemanagement/NodeNetworkManager.java
+++ src/main/java/com/bbn/protelis/networkresourcemanagement/NodeNetworkManager.java
@@ -212,7 +212,7 @@ public class NodeNetworkManager implements NetworkManager {
                     } // while !interrupted accept connections
                 } catch (final IOException e) {
                     LOGGER.warn("Node: " + node.getName()
-                            + " received I/O exception accepting connections, trying to listen again", e);
+                            + " received I/O exception accepting connections, trying to listen again on port: " + port, e);
                 }
             } // while !interrupted, restart listen
 
