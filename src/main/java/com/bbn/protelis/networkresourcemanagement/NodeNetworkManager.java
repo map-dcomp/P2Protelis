aadeb091cd90456bf73d045fa1adcd91a546a584
Jon Schewe <jon.schewe@raytheon.com>
2017-08-25 14:30:37 -0500

    Remove unused imports

--- src/main/java/com/bbn/protelis/networkresourcemanagement/NodeNetworkManager.java
+++ src/main/java/com/bbn/protelis/networkresourcemanagement/NodeNetworkManager.java
@@ -10,16 +10,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
 
-import javax.annotation.Nonnull;
-
 import org.protelis.lang.datatype.DeviceUID;
 import org.protelis.lang.datatype.Tuple;
 import org.protelis.vm.NetworkManager;
 import org.protelis.vm.util.CodePath;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.google.common.collect.ImmutableMap;
 
 /**
  * Network manager for a {@link NetworkServer}.
